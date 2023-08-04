14c15dd9cad9e5ffccac093d1011941a0e1b533d
RODO.L <wuaicot8@gmail.com>
2023-08-04 16:21:32 -0400

    more cors

--- server.js
+++ server.js
@@ -9,13 +9,18 @@ const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
-
 app.use(cors({
   origin: 'https://capable-platypus-f7f625.netlify.app',
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://capable-platypus-f7f625.netlify.app");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(contactRoute);
 
