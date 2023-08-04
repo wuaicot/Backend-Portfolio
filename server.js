fb26064c87c6d83de412520e51ca32294d185060
RODO.L <wuaicot8@gmail.com>
2023-08-04 16:34:47 -0400

    coorss

--- server.js
+++ server.js
@@ -16,12 +16,6 @@ app.use(cors({
   credentials: true,
 }));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://capable-platypus-f7f625.netlify.app");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 app.use(contactRoute);
 
 app.listen(PORT, () => {
