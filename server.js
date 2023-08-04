a44a07dab1491ed4fc375323138ee74e5f5f9b30
RODO.L <wuaicot8@gmail.com>
2023-08-04 18:58:15 -0400

    coño

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
 app.use(cors({
   origin: process.env.FRONTEND_URL,
   methods: ['GET', 'POST'],
