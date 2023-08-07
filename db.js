db87bd7b6e8f41f09cad114c1e5a2fea7586fbf7
RODO.L <wuaicot8@gmail.com>
2023-08-07 16:37:44 -0400

    modifying environment variable in db

--- db.js
+++ db.js
@@ -2,11 +2,27 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL, // Utiliza la variable de entorno DATABASE_URL
+  ssl: {
+    rejectUnauthorized: false, // Solo si estás teniendo problemas con SSL en tu entorno local
+  },
 });
 
 module.exports = pool;
+
+
+
+
+
+// const { Pool } = require('pg');
+// require('dotenv').config();
+
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_DATABASE,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
+
+// module.exports = pool;
