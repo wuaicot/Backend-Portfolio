8848fd33a63f74714b74bc4826c8a97e99c7a080
RODO.L <wuaicot8@gmail.com>
2023-08-07 18:24:09 -0400

    adding cors polityc

--- server.js
+++ server.js
@@ -1,23 +1,42 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require("morgan");
 const cors = require('cors');
 const   contactRoute  = require('./externed');
+const { NETLIFY_DOMAIN } = process.env;
 require('dotenv').config();
+require('./db')
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(morgan("dev"));
 
-app.use(cors({
-   
-  origin: process.env.NETLIFY_DOMAIN,
-  methods: ['GET', 'POST'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-}));
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", `${ NETLIFY_DOMAIN }`);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.setHeader("Permissions-Policy", "ch-ua-form-factor");
+  next();
+});
+app.use(cors());
 app.use('/externed',contactRoute);
+
+ app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+});
+
+module.exports = app;
+
  
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
