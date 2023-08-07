f922e2653371ca02cb69aacc2638e9590a726a92
RODO.L <wuaicot8@gmail.com>
2023-08-07 01:16:02 -0400

    armando rompecabezas

--- externed.js
+++ externed.js
@@ -1,26 +1,25 @@
-const { Router } = require('express');
-const { createTransport } = require('nodemailer');
-const { google } = require('googleapis');
-const { OAuth2 } = google.auth;
-const ContactMessage = require('./models/ContactMessage.js');
-const dotenv = require('dotenv');
-dotenv.config();
-
-const router = Router();
+const express = require('express');
+const router = express.Router();
+const nodemailer = require('nodemailer');
+const { google } = require('googleapis')
+const { OAuth2 } = google.auth.OAuth2; 
+const ContactMessage = require('./models/ContactMessage');
+require('dotenv').config();
 
 const sendEmail = async (name, email, message) => {
   try {
-    const transporter = createTransport({
+    const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        type: 'OAuth2',
+        type: OAuth2,
         user: process.env.EMAIL_USER,
         clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-        refreshToken: process.env.REFRESH_TOKEN,
+        accessToken: process.env.ACCESS_TOKEN,     
+        expires: 3599,
       },
     });
 
+    // Envía el correo electrónico
     await transporter.sendMail({
       from: 'wuaicot8@gmail.com',
       to: email,
@@ -34,33 +33,99 @@ const sendEmail = async (name, email, message) => {
   }
 };
 
+
 router.post('/contact', async (req, res) => {
   try {
     const { name, email, message } = req.body;
 
     // Crea un nuevo registro en la base de datos utilizando el modelo "ContactMessage"
-    const newContactMessage = await ContactMessage.create({
-      name,
-      email,
-      message,
-    });
+    const newContactMessage = await ContactMessage.create(name, email, message);
 
     // Envía el correo electrónico
     sendEmail(name, email, message);
 
     // Responde con un mensaje de éxito
-    res
-      .status(200)
-      .json({
-        message:
-          'Message received successfully! Check your email for confirmation.',
-      });
+    res.status(200).json({ message: 'Message received successfully! Check your email for confirmation.' });
   } catch (error) {
     console.error('Error sending message:', error);
-    res.status(500).json({
-      message: 'Error sending message. Please try again later.',
-    });
+    res.status(500).json({ message: 'Error sending message. Please try again later.' });
   }
 });
 
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+// const { Router } = require('express');
+// const { createTransport } = require('nodemailer');
+// const { google } = require('googleapis');
+// const { OAuth2 } = google.auth;
+// const ContactMessage = require('./models/ContactMessage.js');
+// const dotenv = require('dotenv');
+// dotenv.config();
+
+// const router = Router();
+
+// const sendEmail = async (name, email, message) => {
+//   try {
+//     const transporter = createTransport({
+//       service: 'gmail',
+//       auth: {
+//         type: 'OAuth2',
+//         user: process.env.EMAIL_USER,
+//         clientId: process.env.CLIENT_ID,
+//         clientSecret: process.env.CLIENT_SECRET,
+//         refreshToken: process.env.REFRESH_TOKEN,
+//       },
+//     });
+
+//     await transporter.sendMail({
+//       from: 'wuaicot8@gmail.com',
+//       to: email,
+//       subject: 'Mensaje recibido',
+//       text: `Hola ${name},\n\nGracias por contactarme. Su solictud sera revisada, y me prondre en contacto con usted  \n\nMensaje: ${message}`,
+//     });
+
+//     console.log('Correo enviado correctamente.');
+//   } catch (error) {
+//     console.error('Error al enviar el correo:', error);
+//   }
+// };
+
+// router.post('/contact', async (req, res) => {
+//   try {
+//     const { name, email, message } = req.body;
+
+//     // Crea un nuevo registro en la base de datos utilizando el modelo "ContactMessage"
+//     const newContactMessage = await ContactMessage.create({
+//       name,
+//       email,
+//       message,
+//     });
+
+//     // Envía el correo electrónico
+//     sendEmail(name, email, message);
+
+//     // Responde con un mensaje de éxito
+//     res
+//       .status(200)
+//       .json({
+//         message:
+//           'Message received successfully! Check your email for confirmation.',
+//       });
+//   } catch (error) {
+//     console.error('Error sending message:', error);
+//     res.status(500).json({
+//       message: 'Error sending message. Please try again later.',
+//     });
+//   }
+// });
+
+// module.exports = router;
--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 const express = require('express');
-const { json } = require('body-parser');
+const bodyParser = require('body-parser');
 const cors = require('cors');
-const   router  = require('./externed');
+const   contactRoute  = require('./externed');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(json());
+app.use(bodyParser.json());
 
 app.use(cors({
     
-  origin: 'http://localhost:3000/contact',
+  origin: 'http://localhost:3000',
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }));
 
-app.use('/contact', router);
+app.use('/externed',contactRoute);
  
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
