4c81ec14f7a09d75b81bb1da011eb275a37b0336
RODO.L <wuaicot8@gmail.com>
2023-08-17 15:59:35 -0400

    changin to 3005

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const   contactRoute  = require('./externed');
 require('./db')
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3005;
 
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(bodyParser.json({ limit: "50mb" }));
