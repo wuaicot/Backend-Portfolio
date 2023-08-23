d055dfd4dfaf6f4d48d2adaf14853b6fb0aa16d3
RODO.L <wuaicot8@gmail.com>
2023-08-22 20:02:56 -0400

    con sequelize

--- db.js
+++ db.js
@@ -1,29 +1,13 @@
-// const { Sequelize } = require('sequelize');
-// require('dotenv').config();
-
-// const sequelize = new Sequelize(process.env.DATABASE_URL, {
-//   dialect: 'postgres',
-//   // Otras opciones de configuración...
-// });
-
-// // Importar y configurar tus modelos aquí...
-
-// // Sincronización y migraciones aquí...
-
-// module.exports = sequelize;
-
-
-
-
-const { Pool } = require('pg');
+const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const pool = new Pool({
-  user: process.env.DB_USER,
+const sequelize = new Sequelize({
+  dialect: 'postgres',
   host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
-module.exports = pool;
+module.exports = sequelize;
