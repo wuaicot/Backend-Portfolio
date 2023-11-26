6199d17e9e7268a0f11423bd54c8b2f3bf427feb
RODO.L <wuaicot8@gmail.com>
2023-11-26 15:24:00 -0300

    msi

--- db.js
+++ db.js
@@ -12,16 +12,13 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-
 console.log('DB_USER:', process.env.DB_USER);
 console.log('DB_HOST:', process.env.DB_HOST);
 console.log('DB_DATABASE:', process.env.DB_DATABASE);
 console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
 console.log('DB_PORT:', process.env.DB_PORT);
 
-
 const pool = new Pool({
-
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
@@ -29,7 +26,6 @@ const pool = new Pool({
   port: process.env.DB_PORT
 });
 
-module.exports = pool;
 
 
 
