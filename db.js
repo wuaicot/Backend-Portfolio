2408d1ccf6037a19818195766c7ced4c1f24f430
RODO.L <wuaicot8@gmail.com>
2023-11-26 12:28:33 -0300

    /database

--- db.js
+++ db.js
@@ -15,11 +15,11 @@ require('dotenv').config();
 
 const pool = new Pool({
 
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+   //database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.DPGPORT
 });
 
 module.exports = pool;
