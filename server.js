bcb7420a6f012c72ca670142b292a81175a87378
RODO.L <wuaicot8@gmail.com>
2023-11-26 12:25:15 -0300

    error on .json

--- server.js
+++ server.js
@@ -49,10 +49,10 @@ app.use('/externed', contactRoute);
 
 app.use((err, req, res, next) => {
   const status = err.status || 500;
-  const message = err.message || err;
   console.error(err);
-  res.status(status).send(message);
+  res.status(status).json({ error: err.message || 'Internal Server Error' });
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
