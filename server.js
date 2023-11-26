53f4c01575165f07aaa05111a925ad9433d63300
RODO.L <wuaicot8@gmail.com>
2023-11-25 21:30:13 -0300

    C:/Program Files/Git/

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(morgan('dev'));
 //http://localhost:3000
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://frontend-portfolio-production.up.railway.app');
+  res.header('Access-Control-Allow-Origin', 'https://frontend-portfolio-production.up.railway.app/');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
