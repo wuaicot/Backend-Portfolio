d1eb77b80941a41f4c0bfaf1b7b944aa118d274d
Naycol Rodolfo Linares Villasmil <wuaicot8@gmail.com>
2024-10-13 01:03:01 -0300

    va bien, segir...

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const contactRoute = require('./externed');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.DB_PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 app.use(bodyParser.json({ limit: '50mb' }));
