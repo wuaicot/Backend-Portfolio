106fd367515f453be79af9311fef9d3d2153192b
Naycol Rodolfo Linares Villasmil <wuaicot8@gmail.com>
2024-10-14 06:33:44 -0300

    *

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use(morgan('dev'));
 //https://frontend-portfolio-production.up.railway.app/
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://frontend-portfolio-production.up.railway.app');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
